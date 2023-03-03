80837ede1252920a013813e2f8422b829bb11c9b
Félicien Corbat <felicien@champis.net>
2023-03-03 16:12:21 +0100

    feat: Add navbar and routes #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { IdentityComponent } from './identity/identity.component';
+import { GoalsComponent } from './goals/goals.component';
+import { EnvironmentComponent } from './environment/environment.component';
+import { ValueComponent } from './value/value.component';
+import { MeComponent } from './me/me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IdentityComponent,
+    GoalsComponent,
+    EnvironmentComponent,
+    ValueComponent,
+    MeComponent
   ],
   imports: [
     BrowserModule,
